6250f8347b70e25f896c745eb3ac3db3137532ae
Alexander Hausmann <email@alexh.rocks>
2025-10-15 23:04:07 +0200

    bump api version

--- src/generated/rest_api_structure.ts
+++ src/generated/rest_api_structure.ts
@@ -3,7 +3,7 @@ import { integer, ApiResponse_AuthLoginPost, ApiResponse_AuthLoginwithtokenPost,
 
 export function makeStructure(makeRequest: makeRequestFunctionType) {
     return {
-        API_VERSION: '0.14.0'
+        API_VERSION: '0.15.0'
         ,
         auth: {
             login: {
